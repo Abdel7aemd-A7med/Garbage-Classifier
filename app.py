0d77f7de8a05650c6db7c06eee696847fc15daa9
Abdelhamed Ahmed <abdelhamedahmed45e@gmail.com>
2025-08-05 07:33:01 +0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,39 @@
+import gradio as gr
+import numpy as np
+from PIL import Image
+import tensorflow as tf
+
+model = tf.keras.models.load_model("Garbage_1.h5")  
+
+class_names = [
+    "battery", "biological", "cardboard", "clothes", "glass",
+    "metal", "paper", "plastic", "shoes", "trash"
+]
+
+def predict(image):
+    image = image.resize((224, 224))  
+    image = np.array(image)
+
+    if image.ndim == 2:  # grayscale
+        image = np.stack((image,) * 3, axis=-1)
+    elif image.shape[-1] != 3:
+        image = image[..., :3]  
+
+    image = image.astype(np.float32) / 255.0  
+    image = np.expand_dims(image, axis=0)     
+
+    pred = model.predict(image)
+    class_idx = np.argmax(pred[0])
+    confidence = float(np.max(pred[0]))
+
+    return f"Prediction: {class_names[class_idx]} ({confidence * 100:.2f}%)"
+
+interface = gr.Interface(
+    fn=predict,
+    inputs=gr.Image(type="pil"),
+    outputs="text",
+    title="Waste Classification",
+    description="ارفع صورة وسيقوم النموذج بتصنيف نوع النفاية بدقة"
+)
+
+interface.launch(share=True)
